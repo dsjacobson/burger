2007e601de925b9779e8cc098975c16721806034
unknown <you@example.com>
2018-03-07 15:39:14 -0600

    almost complete, missing styling

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Double Bacon Cheeseburger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Triple-Stacked Bacon CheeseBurger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("McChicken Cheeseburger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Veggie Burger", true);
